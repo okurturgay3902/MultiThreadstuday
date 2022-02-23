bc049d631e2f55f5d4ffe500c79cd31939c42a05
okurturgay3902 <94409068+okurturgay3902@users.noreply.github.com>
2022-02-23 18:58:57 +0100

    ders basi

---
+++ src/main/java/com/example/multithreadstuday/MultiThreadStudayApplication.java
@@ -0,0 +1,13 @@
+package com.example.multithreadstuday;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MultiThreadStudayApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MultiThreadStudayApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/multithreadstuday/MultiThreadStudayApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.multithreadstuday;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MultiThreadStudayApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
